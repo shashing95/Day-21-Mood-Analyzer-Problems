d9922e98832488c5e3ea10a59723e87afca1d07d
shashi <shashidharngg@gmail.com>
2022-09-01 20:42:19 +0530

    added:Program for the Create the object the mood analyser

---
+++ src/com/bridgelabz/MoodAnalyzer/MoodAnalyzer.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.MoodAnalyzer;
+
+public class MoodAnalyzer {
+	  public MoodAnalyzer() {
+	    }
+
+	    public String analyzerMood(String message) {
+	        if (message.contains("Sad"))
+	            return "SAD";
+	        else
+	            return "HAPPY";
+	    }
+	}
+
+
